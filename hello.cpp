56c68824fb5e1c94141d60001d9942fb17a95a47
viv <demo123@gmail.com>
2026-08-18 10:31:07 +0530

    cpp file some code

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void printHello(){
+    cout<<"Hello World!!!!!!!!!!!!!!!!";
+}
+
+int main(){
+
+    printHello();
+
+
+    int a, b;
+    cout<<"enter two numbers: \n";
+    cin>>a>>b;
+
+    return a+b;
+}
